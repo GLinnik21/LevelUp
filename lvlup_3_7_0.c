11487d59ad627e05a190daadb30d38bf47055a26
Gleb Linnik <gleb_linnik@icloud.com>
2016-11-03 20:07:14 +0300

    7-th task is ready

--- lvlup_3_7_0.c
+++ lvlup_3_7_0.c
@@ -1,4 +1,21 @@
-//
-// Created by Глеб Линник on 03.11.16.
-//
+#include <stdio.h>
+#include <memory.h>
+
+
+
+int main() {
+    char stringSrs[256];
+    char stringDest[512];
+
+    fgets(stringDest, 256, stdin);
+    fgets(stringSrs, 256, stdin);
+
+    stringDest[strlen(stringDest) - 1] = 0;
+
+    strcat (stringDest, stringSrs);
+
+    printf("%s\n", stringDest);
+
+    return 0;
+}
 
