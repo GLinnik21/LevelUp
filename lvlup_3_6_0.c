122c1200d55b8f617184702a9505522c621d3d5f
Gleb Linnik <gleb_linnik@icloud.com>
2016-11-03 13:12:34 +0300

    7-th task is ready

---
+++ lvlup_3_6_0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <memory.h>
+
+int main() {
+    char stringSrs[256];
+    char stringTrgt[256];
+    fgets(stringSrs, 256, stdin);
+
+    for (int i = 0, j = strlen(stringSrs) - 2; i < strlen(stringSrs) - 1; ++i, --j) {
+        stringTrgt[i] = stringSrs[j];
+    }
+
+    printf("%s\n", stringTrgt);
+
+    return 0;
+}
