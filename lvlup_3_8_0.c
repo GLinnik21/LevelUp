b2f102894c5c64a1e954860c5f7c6b801cfd8642
Gleb Linnik <gleb_linnik@icloud.com>
2016-11-04 13:51:49 +0300

    8-th task ready

---
+++ lvlup_3_8_0.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <memory.h>
+
+const char* strcut(char*, const char*);
+
+int main () {
+    char first[128];
+    char second[128];
+    fgets(first, 128, stdin);
+    fgets(second, 128, stdin);
+    const char* str = strcut(first, second);
+    printf("%s", str);
+    return 0;
+}
+
+const char* strcut(char* str, const char* find) {
+    for (int i = 0; i < strlen(str); ++i) {
+        if (str[i] == find[0]) {
+            for (int j = 0; j < strlen(find); ++j, ++i) {
+                if ((str[i] == find[j]) || (str[i] == '\0')) {
+                    str[i] = (char)127;
+                } else {
+                    i--;
+                    break;
+                }
+            }
+        }
+    }
+    char newStr[128] = {0};
+    for (int i = 0, j = 0; i < strlen(str); ++i) {
+        if (str[i] == (char)127) {
+            continue;
+        }
+        newStr[j] = str[i];
+        ++j;
+    }
+    return newStr;
+}
