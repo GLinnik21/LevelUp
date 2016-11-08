833764699c24fd3b382f2fce18d57882a8ea9b0f
Gleb Linnik <gleb_linnik@icloud.com>
2016-11-08 21:59:09 +0300

    9-th task fix

--- lvlup_3_9_0.c
+++ lvlup_3_9_0.c
@@ -1,33 +1,49 @@
 #include <stdio.h>
 #include <memory.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <locale.h>
 
-void swap(char*, char*);
+#define ARRAY_SIZE 10
+
+void swap(char **, char **);
 
 int main() {
-    char string[256];
-    fgets(string, 256, stdin);
-    int n = strlen(string);
+    setlocale(LC_ALL, "Russian");
+    char *strings[ARRAY_SIZE];
+
+    printf("Введите строки для сортировки: \n");
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        strings[i] = (char *) calloc(256, sizeof(char));
+        printf("(%d): ", i + 1);
+        fgets(strings[i], 256, stdin);
+        strings[i][strlen(strings[i]) - 1] = 0;
+    }
+
     bool swapped = false;
 
+    int n = ARRAY_SIZE;
     do {
         swapped = false;
         for (int i = 1; i <= n - 1; ++i) {
-            if ((int)string[i - 1] > (int)string[i]) {
-                swap(&string[i - 1], &string[i]);
+            if (strcmp(strings[i - 1], strings[i]) > 0) {
+                swap(&strings[i - 1], &strings[i]);
                 swapped = true;
             }
         }
         n--;
     } while (swapped);
 
-    printf("%s", string);
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        printf("%s\n", strings[i]);
+    }
+
 
     return 0;
 }
 
-void swap(char* a, char* b) {
-    char tmp = *a;
+void swap(char **a, char **b) {
+    char *tmp = *a;
     *a = *b;
     *b = tmp;
 }
