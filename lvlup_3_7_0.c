ea7d10cd01c1b7606f31e514fb1cc904f7134353
Gleb Linnik <gleb_linnik@icloud.com>
2016-11-08 19:30:50 +0300

    7-th task fix

--- lvlup_3_7_0.c
+++ lvlup_3_7_0.c
@@ -4,13 +4,14 @@
 
 
 int main() {
-    char stringSrs[256];
-    char stringDest[512];
+    char stringSrs[256] = {0};
+    char stringDest[512] = {0};
 
     fgets(stringDest, 256, stdin);
     fgets(stringSrs, 256, stdin);
 
     stringDest[strlen(stringDest) - 1] = 0;
+    stringDest[strlen(stringSrs) - 1] = 0;
 
     strcat (stringDest, stringSrs);
 
