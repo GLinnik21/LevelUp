d46c58b0f620e6eeac33eae144e0b48a602c3801
Gleb Linnik <gleb_linnik@icloud.com>
2016-11-08 19:18:14 +0300

    5-th task fix (no Russian in gcc try on VS compiler)

--- lvlup_3_5_0.c
+++ lvlup_3_5_0.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <memory.h>
+#include <locale.h>
 
 int main() {
-    char string[256], subString[256];
+    setlocale(LC_ALL, "Russian");
+    char string[256] = {0}, subString[256] = {0};
 
     fgets(string, 256, stdin);
     fgets(subString, 256, stdin);
+    string[(int)strlen(string) - 1] = 0;
+    subString[(int)strlen(subString) - 1] = 0;
 
     printf("Индексы совпадений: ");
 
@@ -13,8 +17,8 @@ int main() {
 
     for (int i = 0; i < strlen(string); ++i) {
         if (string[i] == subString[0]) {
-            for (int j = 0; j < strlen(subString); ++j, ++i) {
-                if ((string[i] == subString[j]) || (string[i] == '\0')) {
+            for (int j = 0; j < strlen(subString) + 1; ++j, ++i) {
+                if ((string[i] == subString[j]) && (string[i] != '\0')) {
                     printf("%d,", i);
                 } else {
                     printf(" ");
