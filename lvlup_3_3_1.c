290001d992d139a27bd4c32bfea38958e83a3ea6
Gleb Linnik <gleb_linnik@icloud.com>
2016-11-10 09:30:30 +0300

    3-rd task 2-nd part ready

---
+++ lvlup_3_3_1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void swap(char **, char **);
+
+int main() {
+    char str[256] = {0};
+    printf("Введите строку для нахождения количесва слов: ");
+    fgets(str, 256, stdin);
+    str[strlen(str) - 1] = 0;
+
+    char **stringArr = (char **) malloc(sizeof(void *) * 256);
+
+    //Get thr first token
+    char *token = strtok(str, " ");
+
+    int wordsFound = 0;
+    //Walk through other tokens
+    while (token != NULL) {
+        if (isalpha(token[0])) {
+            stringArr[wordsFound] = token;
+            wordsFound++;
+        }
+        token = strtok(NULL, " ");
+    }
+    printf("Найдено слов: %d\n", wordsFound);
+
+    bool swapped = false;
+
+    int n = wordsFound;
+    do {
+        swapped = false;
+        for (int i = 1; i <= n - 1; ++i) {
+            if (strcmp(stringArr[i - 1], stringArr[i]) > 0) {
+                swap(&stringArr[i - 1], &stringArr[i]);
+                swapped = true;
+            }
+        }
+        n--;
+    } while (swapped);
+
+    for (int i = 0; i < wordsFound; ++i) {
+        printf("%s\n", stringArr[i]);
+    }
+
+    return 0;
+}
+
+void swap(char **a, char **b) {
+    char *tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
