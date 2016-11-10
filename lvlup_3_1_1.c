4651035a70b03e7555da6da0bcf602ba98ad5c93
Gleb Linnik <gleb_linnik@icloud.com>
2016-11-10 08:57:35 +0300

    1-st task 2-nd part

---
+++ lvlup_3_1_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[256] = {0};
+    printf("Введите строку для нахождения количесва слов: ");
+    fgets(str, 256, stdin);
+    str[strlen(str) - 1] = 0;
+
+    //Get thr first token
+    char *token = strtok(str, " ");
+
+    int wordsFound = 0;
+    //Walk through other tokens
+    while (token != NULL) {
+        token = strtok(NULL, " ");
+        if (token != NULL)
+            if (isalpha(token[0])) {
+                printf("%s\n", token);
+                wordsFound++;
+            }
+    }
+    printf("Найдено слов: %d", wordsFound);
+    return 0;
+}
